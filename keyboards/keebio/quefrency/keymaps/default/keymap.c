717fe8541de76bc917213a22243a187a06b01c84
Arnaud Rebts <kid@nas.lan>
2018-11-01 19:44:40 +0100

    Few improvements to Quefrency layout
    - Switches ALT and WIN keys
    - Page Up and Page down maps to Home and End on FN1 layer

--- keyboards/keebio/quefrency/keymaps/default/keymap.c
+++ keyboards/keebio/quefrency/keymaps/default/keymap.c
@@ -22,14 +22,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, \
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_PGUP, \
-    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  MO(_FN1),         KC_SPC,  MO(_FN1),KC_RALT, KC_RGUI, KC_LCTL, MO(_FN1), KC_PGDOWN
+    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  MO(_FN1),         KC_SPC,  MO(_FN1),KC_RGUI, KC_RALT, KC_LCTL, MO(_FN1), KC_PGDOWN
   ),
 
   [_FN1] = LAYOUT(
-    KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,  KC_BSPC, \
-    RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, \
-    _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, \
-    KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,  _______
+    KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
+    RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, \
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_HOME, \
+    KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,  KC_END
   )
 };
