164637fbdf2b8491368d55f3d11f6d1abc750ebe
Arnaud Rebts <arnaud.rebts@gmail.com>
2018-11-06 10:09:56 +0100

    Introduce secondary layer for Mac

--- keyboards/keebio/quefrency/keymaps/default/keymap.c
+++ keyboards/keebio/quefrency/keymaps/default/keymap.c
@@ -8,6 +8,8 @@ extern keymap_config_t keymap_config;
 // entirely and just use numbers.
 #define _BASE 0
 #define _FN1 1
+#define _MAC 2
+#define _FN2 4
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -18,18 +20,34 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  _______, KC_BSPC, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, \
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_PGUP, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  MO(_FN1),         KC_SPC,  MO(_FN1),KC_RGUI, KC_RALT, KC_LCTL, MO(_FN1), KC_PGDOWN
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  _______, KC_BSPC, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, \
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_PGUP, \
+    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  MO(_FN1),          KC_SPC,  MO(_FN1),KC_RGUI, KC_RALT, KC_LCTL, MO(_FN1), KC_PGDOWN
   ),
 
   [_FN1] = LAYOUT(
-    KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
-    RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, \
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_HOME, \
-    KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,  KC_END
+    KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
+    RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______,  _______, _______, _______, _______, _______, _______,  _______, _______, \
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
+    _______, _______, _______, _______, _______, DF(_BASE),_______, DF(_MAC),_______, _______, _______, _______,  KC_HOME, \
+    KC_TILD, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______,  KC_END
+  ),
+
+  [_MAC] = LAYOUT(
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  _______, KC_BSPC, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_PGUP, \
+    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  MO(_FN2),          KC_SPC,  MO(_FN2),KC_RALT, KC_RGUI, KC_LCTL, MO(_FN2), KC_PGDOWN
+  ),
+
+  [_FN2] = LAYOUT(
+    KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
+    RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______,  _______, _______, _______, _______, _______, _______,  _______, _______, \
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
+    _______, _______, _______, _______, _______, DF(_BASE),_______, DF(_MAC),_______, _______, _______, _______,  KC_HOME, \
+    KC_TILD, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______,  KC_END
   )
 };
