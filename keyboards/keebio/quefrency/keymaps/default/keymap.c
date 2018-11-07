63dcdc2cfd832f32157b6da75b5fd33c9df32063
Arnaud Rebts <arnaud.rebts@gmail.com>
2018-11-07 14:45:59 +0100

    Add a game layer that replace the left GUI key with `.`

--- keyboards/keebio/quefrency/keymaps/default/keymap.c
+++ keyboards/keebio/quefrency/keymaps/default/keymap.c
@@ -7,9 +7,11 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _BASE 0
-#define _FN1 1
-#define _MAC 2
-#define _FN2 4
+#define _FN1  1
+#define _MAC  2
+#define _FN2  3
+#define _GAME 4
+#define _FN3  5
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -30,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FN1] = LAYOUT(
     KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
     RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______,  _______, _______, _______, _______, _______, _______,  _______, _______, \
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, DF(_GAME),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
     _______, _______, _______, _______, _______, DF(_BASE),_______, DF(_MAC),_______, _______, _______, _______,  KC_HOME, \
     KC_TILD, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______,  KC_END
   ),
@@ -46,7 +48,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FN2] = LAYOUT(
     KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
     RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______,  _______, _______, _______, _______, _______, _______,  _______, _______, \
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, DF(_GAME),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
+    _______, _______, _______, _______, _______, DF(_BASE),_______, DF(_MAC),_______, _______, _______, _______,  KC_HOME, \
+    KC_TILD, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______,  KC_END
+  ),
+
+  [_GAME] = LAYOUT(
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  _______, KC_BSPC, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, \
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_PGUP, \
+    KC_LCTL, KC_DOT,  KC_LALT, KC_SPC,  MO(_FN3),          KC_SPC,  MO(_FN3),KC_RGUI, KC_RALT, KC_LCTL, MO(_FN3), KC_PGDOWN
+  ),
+
+  [_FN3] = LAYOUT(
+    KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
+    RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______,  _______, _______, _______, _______, _______, _______,  _______, _______, \
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, DF(_GAME),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
     _______, _______, _______, _______, _______, DF(_BASE),_______, DF(_MAC),_______, _______, _______, _______,  KC_HOME, \
     KC_TILD, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______,  KC_END
   )
