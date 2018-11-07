62d604e61f01629d2e3002429c68fefbc140bf23
Arnaud Rebts <arnaud.rebts@gmail.com>
2018-11-07 17:04:05 +0100

    Add volume control to the mac layer

--- keyboards/keebio/quefrency/keymaps/default/keymap.c
+++ keyboards/keebio/quefrency/keymaps/default/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FNM] = LAYOUT(
     KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
-    RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______,  _______, _______, _______, _______, _______, _______,  _______, _______, \
+    RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______,  _______, _______, _______, _______, _______, KC__VOLDOWN,KC__VOLUP, KC__MUTE, \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, DF(_GAME),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
     _______, _______, _______, _______, _______, DF(_BASE),_______, DF(_MAC),_______, _______, _______, _______,  KC_HOME, \
     KC_TILD, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______,  KC_END
