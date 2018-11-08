6325d61ab27eb17e1abbd66541a95bcec9e2f896
Arnaud Rebts <arnaud.rebts@gmail.com>
2018-11-08 17:46:09 +0100

    Use KC_PGDN instead of KC_PGDOWN for readability

--- keyboards/keebio/quefrency/keymaps/default/keymap.c
+++ keyboards/keebio/quefrency/keymaps/default/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, \
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_PGUP, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  TT(_FNB),          KC_SPC,  TT(_FNB),KC_RGUI, KC_RALT, KC_LCTL, TT(_FNB), KC_PGDOWN
+    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  TT(_FNB),          KC_SPC,  TT(_FNB),KC_RGUI, KC_RALT, KC_LCTL, TT(_FNB), KC_PGDN
   ),
 
   [_FNB] = LAYOUT(
@@ -37,12 +37,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______,  KC_END
   ),
 
+
   [_MAC] = LAYOUT(
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  _______, KC_BSPC, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_PGUP, \
-    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  TT(_FNM),          KC_SPC,  TT(_FNM),KC_RALT, KC_RGUI, KC_LCTL, TT(_FNM), KC_PGDOWN
+    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  TT(_FNM),          KC_SPC,  TT(_FNM),KC_RALT, KC_RGUI, KC_LCTL, TT(_FNM), KC_PGDN
   ),
 
   [_FNM] = LAYOUT(
@@ -53,12 +54,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______,  KC_END
   ),
 
+
   [_GAME] = LAYOUT(
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,  _______, KC_BSPC, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, \
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_PGUP, \
-    KC_LCTL, KC_F13,  KC_LALT, KC_SPC,  TT(_FNG),          KC_SPC,  TT(_FNG),KC_RGUI, KC_RALT, KC_LCTL, TT(_FNG), KC_PGDOWN
+    KC_LCTL, KC_F13,  KC_LALT, KC_SPC,  TT(_FNG),          KC_SPC,  TT(_FNG),KC_RGUI, KC_RALT, KC_LCTL, TT(_FNG), KC_PGDN
   ),
 
   [_FNG] = LAYOUT(
