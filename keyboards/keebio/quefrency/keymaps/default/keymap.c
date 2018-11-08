cb8a442dc81fc537ee20c69524f28e24f8c5f0f9
Arnaud Rebts <arnaud.rebts@gmail.com>
2018-11-08 14:03:34 +0100

    Fn + Esc send `

--- keyboards/keebio/quefrency/keymaps/default/keymap.c
+++ keyboards/keebio/quefrency/keymaps/default/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FNB] = LAYOUT(
-    KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
+    KC_GRAVE,KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
     RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______,  _______, _______, _______, _______, _______, _______,  _______, _______, \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, DF(_GAME),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
     _______, _______, _______, _______, _______, DF(_BASE),_______, DF(_MAC),_______, _______, _______, _______,  KC_HOME, \
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FNM] = LAYOUT(
-    KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
+    KC_GRAVE,KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
     RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______,  _______, _______, _______, _______, _______, KC__VOLDOWN,KC__VOLUP, KC__MUTE, \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, DF(_GAME),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
     _______, _______, _______, _______, _______, DF(_BASE),_______, DF(_MAC),_______, _______, _______, _______,  KC_HOME, \
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FNG] = LAYOUT(
-    KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
+    KC_GRAVE,KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
     RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______,  _______, _______, _______, _______, _______, _______,  _______, _______, \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, DF(_GAME),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  _______,\
     _______, _______, _______, _______, _______, DF(_BASE),_______, DF(_MAC),_______, _______, _______, _______,  KC_HOME, \
