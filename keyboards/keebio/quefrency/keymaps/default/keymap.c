80f652f4e64f8dfccc148689fe7880240f3e7cc9
Arnaud Rebts <arnaud.rebts@gmail.com>
2019-03-21 09:58:44 +0100

    Adds media buttons

--- keyboards/keebio/quefrency/keymaps/default/keymap.c
+++ keyboards/keebio/quefrency/keymaps/default/keymap.c
@@ -50,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FNM] = LAYOUT(
     KC_GRAVE,KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  _______,  KC_DELETE, \
-    RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______,  _______, _______, _______, _______, _______, KC__VOLDOWN,KC__VOLUP, KC__MUTE, \
+    RGB_TOG, RGB_MOD, KC_UP,   _______, _______, _______,  _______, _______, _______, _______, KC_MRWD, KC__VOLDOWN,KC__VOLUP, KC__MUTE, \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, DF(_GAME),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,  KC_INS, \
-    _______, _______, _______, _______, _______, DF(_BASE),_______, DF(_MAC),_______, _______, _______, _______,  KC_HOME, \
-    KC_TILD, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______,  KC_END
+    _______, _______, _______, _______, _______, DF(_BASE),KC_MFFD, DF(_MAC),_______, _______, _______, _______,  KC_HOME, \
+    KC_TILD, _______, _______, KC_MPLY, _______,           KC_MPLY, _______, _______, _______, _______, _______,  KC_END
   ),
 
 
